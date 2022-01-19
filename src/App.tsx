3c9c303d0e67352ac8185c75af72f6a2384e9d97
Pyoungwoo <pyoungwoo921105@gmail.com>
2022-01-19 16:43:18 +0900

    [feat] OCR 도입

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const { createWorker, PSM } = require("tesseract.js");
+  const worker = createWorker();
+
+  const [imagePath, setImagePath] = useState("");
+  const [text, setText] = useState("");
+
+  const handleChange = (event: any) => {
+    setImagePath(URL.createObjectURL(event.target.files[0]));
+  };
+
+  const handleClick = async () => {
+    await worker.load();
+    await worker.loadLanguage("kor+eng");
+    await worker.initialize("kor+eng");
+    await worker.setParameters({
+      tessedit_pageseg_mode: PSM.SINGLE_BLOCK,
+    });
+    const {
+      data: { text },
+    } = await worker.recognize(imagePath);
+    console.log(text);
+    setText(text);
+    await worker.terminate();
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main className="App-main">
+        <h3>Actual imagePath uploaded</h3>
+        <img src={imagePath} className="App-image" alt="" />
+
+        <h3>Extracted text</h3>
+        <div className="text-box">
+          <p> {text} </p>
+        </div>
+        <input type="file" onChange={handleChange} />
+        <button onClick={handleClick} style={{ height: 50 }}>
+          {" "}
+          convert to text
+        </button>
+      </main>
     </div>
   );
 }
